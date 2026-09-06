5793bb5cb07f6359d4563f21ee1eb2bfbbb4c340
john90921 <sorvictor90@gmail.com>
2026-09-06 13:34:01 +0800

    feat:font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,42 @@
 import '../global.css';
 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack, Tabs } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { SplashScreen } from 'expo-router';
+import {SafeAreaView as RNSSafeAreaView} from 'react-native-safe-area-context';
+import { styled } from 'nativewind';
+const SafeAreaView =styled(RNSSafeAreaView);
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
   );
 }
